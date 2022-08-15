dfa54c39f947e0cf23963ac11b0f762618497f32
KeenIIDX <elliott@wilcoxon.org>
2022-08-14 20:12:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# openFPGA Updater
+
+This Windows Powershell script automates downloading all of the currently-available cores for the [Analogue Pocket's](https://www.analogue.co/pocket) OpenFPGA feature.  At present it's a very basic script that simply downloads every core every time you run it, I hope to improve it in future versions to skip cores that do not need updates.
+
+## Setup
+If you have not run the script before, open a Powershell prompt and run:
+    PS> Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+Download the Powershell script [here]() and place it on the root of your Pocket's micro SD card.  Right click the file, click 'Run with PowerShell'.  A window will open as it's running.
+
+## Thanks
+Thank you to Josh Campbell for his [openFPGA Cores Inventory](https://joshcampbell191.github.io/openfpga-cores-inventory/analogue-pocket) page.
+Thank you to Neil Morrison for his [Core updating script](https://gist.github.com/neil-morrison44/34fbb18de90cd9a32ca5bdafb2a812b8), which was the inspiration for this one.
---
+++ openFpgaUpdater.ps1
@@ -0,0 +1,48 @@
+﻿$CoreSource = "https://joshcampbell191.github.io/openfpga-cores-inventory/analogue-pocket"
+
+function Get-MainCoreList {
+    param (
+        $url
+    )
+
+    return Invoke-WebRequest -Uri $url -UseBasicParsing -UserAgent "KeenIIDX"
+}
+
+function Parse-CoreList {
+    param (
+        $content
+    )
+
+    $coreUrls = $content.Links | Where-Object {$_.Href -match "^([^/]*/){4}[^/]*$"} | select -Property href | ForEach-Object {($_.href + "/releases").Replace("//github.com/", "//api.github.com/repos/")}
+
+    return $coreUrls
+}
+
+try {
+    # Grab central list of cores available.  Pluck out the links to their github repositories
+    $cores = Get-MainCoreList -url $CoreSource
+    $cores = Parse-CoreList -content $cores
+} catch {
+    Throw $_
+}
+
+# Grabbing each core.
+Foreach ($link in $cores) {
+    $Response = (Invoke-RestMethod -uri $link -UserAgent "KeenIIDX")[0]
+    Foreach ($file in $Response.assets) {
+        if ($file.browser_download_url -like "*.zip") {
+            $downloadLink = $file.browser_download_url
+            $fileName = $file.name
+            $downloadPath = "$env:TEMP\$fileName"
+
+            #Download the core.
+            Invoke-WebRequest -Uri $downloadLink -OutFile $downloadPath
+
+            #Unzip the core to here.
+            Expand-Archive -Path $downloadPath -DestinationPath .\ -Force
+
+            #Delete the downloaded file.
+            Remove-Item -Path $downloadPath -Force
+        }
+    }
+}
