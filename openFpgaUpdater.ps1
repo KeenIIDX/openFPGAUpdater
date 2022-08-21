71a5b80e96e54cdcfa03e66b196ed74f2d348172
KeenIIDX <elliott@wilcoxon.org>
2022-08-20 21:30:40 -0500

    Add local cache of installed cores.

--- openFpgaUpdater.ps1
+++ openFpgaUpdater.ps1
@@ -1,48 +1,69 @@
 ﻿$CoreSource = "https://joshcampbell191.github.io/openfpga-cores-inventory/analogue-pocket"
+$localCores = "core_repos.json"
 
 function Get-MainCoreList {
     param (
         $url
     )
 
-    return Invoke-WebRequest -Uri $url -UseBasicParsing -UserAgent "KeenIIDX"
+    Invoke-WebRequest -Uri $url -UseBasicParsing -UserAgent "KeenIIDX"
 }
 
-function Parse-CoreList {
+function ConvertTo-CoreList {
     param (
         $content
     )
 
-    $coreUrls = $content.Links | Where-Object {$_.Href -match "^([^/]*/){4}[^/]*$"} | select -Property href | ForEach-Object {($_.href + "/releases").Replace("//github.com/", "//api.github.com/repos/")}
-
-    return $coreUrls
+    $content.Links | Where-Object {$_.Href -match "^([^/]*/){4}[^/]*$"} | select -Property href | ForEach-Object {($_.href + "/releases").Replace("//github.com/", "//api.github.com/repos/")}
 }
 
-try {
-    # Grab central list of cores available.  Pluck out the links to their github repositories
-    $cores = Get-MainCoreList -url $CoreSource
-    $cores = Parse-CoreList -content $cores
-} catch {
-    Throw $_
+# Grab central list of cores available.  Pluck out the links to their github repositories
+$newCores = Get-MainCoreList -url $CoreSource
+$newCores = ConvertTo-CoreList -content $newCores
+
+# Load local list of cores
+$installed = Get-Content -Raw -ErrorAction:Ignore -Path $localCores | ConvertFrom-Json
+
+# Prep list of cores
+if ($installed) {
+    foreach ( $core in $newCores ) {
+        if ( $core -notin $installed.link ) {
+            $installed += [PSCustomObject]@{ 
+                link = $core 
+                version = "0.0.0" 
+            }
+        }
+    }
+} else {
+    $installed = $newCores | foreach { [PSCustomObject]@{ link = $_ ; version = "0.0.0" } }
 }
 
 # Grabbing each core.
-Foreach ($link in $cores) {
-    $Response = (Invoke-RestMethod -uri $link -UserAgent "KeenIIDX")[0]
-    Foreach ($file in $Response.assets) {
-        if ($file.browser_download_url -like "*.zip") {
-            $downloadLink = $file.browser_download_url
-            $fileName = $file.name
-            $downloadPath = "$env:TEMP\$fileName"
+Foreach ($core in $installed) {
+    $Response = (Invoke-RestMethod -uri $core.link -UserAgent "KeenIIDX")[0]
+    $availableVersion = $Response.tag_name -replace( '.*(?<version>\d+\.\d+\.\d+).*', '${version}' )
 
-            #Download the core.
-            Invoke-WebRequest -Uri $downloadLink -OutFile $downloadPath
+    if ( [System.Version]$core.version -lt [System.Version]$availableVersion ) {
+        Foreach ($file in $Response.assets) {
+            if ($file.browser_download_url -like "*.zip") {
+                $downloadLink = $file.browser_download_url
+                $fileName = $file.name
+                $downloadPath = "$env:TEMP\$fileName"
 
-            #Unzip the core to here.
-            Expand-Archive -Path $downloadPath -DestinationPath .\ -Force
+                #Update cached version
+                $core.version = $availableVersion
 
-            #Delete the downloaded file.
-            Remove-Item -Path $downloadPath -Force
+                #Download the core.
+                Invoke-WebRequest -Uri $downloadLink -OutFile $downloadPath
+
+                #Unzip the core to here.
+                Expand-Archive -Path $downloadPath -DestinationPath .\ -Force
+
+                #Delete the downloaded file.
+                Remove-Item -Path $downloadPath -Force
+            }
         }
     }
 }
+
+$installed | ConvertTo-Json -depth 3 | Out-File $localCores
